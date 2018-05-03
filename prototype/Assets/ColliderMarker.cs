95761b20b5ea010bfbadf61d5376414179fd9b8f
Thomas <tpajonck@gmail.com>
2018-05-03 22:33:53 +0200

    basic hit detection

---
+++ prototype/Assets/ColliderMarker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderMarker : MonoBehaviour
+{
+    private GameObject _oldTarget;
+    private MeleeCombat _meleeCombat;
+
+	private void Awake()
+	{
+        _meleeCombat = GetComponentInParent<MeleeCombat>();
+	}
+
+	public void ResetMarker()
+    {
+        _oldTarget = null;
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject != _oldTarget)
+        {
+            _meleeCombat.RegisterHit(other.gameObject);
+            _oldTarget = other.gameObject;
+        }
+	}
+}
---
+++ prototype/Assets/MeleeCombat.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCombat : MonoBehaviour
+{
+	[SerializeField] private ColliderMarker[] _markers;
+
+    private Animator _anim;
+    private bool _isPlaying;
+    private List<GameObject> _oldTargets = new List<GameObject>();
+
+	private void Awake()
+	{
+        _anim = GetComponent<Animator>();
+	}
+
+    public void AnimationState(int animState)
+    {
+        if (animState != 0)
+        {
+            _oldTargets.Clear();
+            ResetMarkers();
+            _isPlaying = true;
+        }
+        else
+        {
+            _isPlaying = false;
+        }
+    }
+
+    private void ResetMarkers()
+    {
+        for (int i = 0; i < _markers.Length; i++)
+        {
+            _markers[i].ResetMarker();
+        }
+    }
+
+    public void RegisterHit(GameObject obj)
+    {
+        if (!_oldTargets.Contains(obj))
+        {
+            Debug.Log("HITTING " + obj);
+            _oldTargets.Add(obj);
+        }
+    }
+
+	private void Update()
+	{
+		_anim.SetTrigger("MeleeAttack");
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+        }
+
+        if (!_isPlaying)
+        {
+            return;
+        }
+	}
+}
